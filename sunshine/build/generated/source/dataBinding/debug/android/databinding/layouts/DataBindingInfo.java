ba6ecf5ef7556b0e30e7212b8b92e3e70812d277
Eman_1111 <eman.ashour1111@gmail.com>
2017-11-13 07:06:50 +0200

    Finish background Jobs

--- sunshine/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ sunshine/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="76fdcb6d-a576-4dee-865e-95576c4630fa")
+@BindingBuildInfo(buildId="67c1a0ca-3f42-45ca-bb71-5ab5fad459a6")
 public class DataBindingInfo {}
