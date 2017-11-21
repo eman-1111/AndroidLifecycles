4cf9f78e53419779d61862fee1534b179eb291cd
Eman_1111 <eman.ashour1111@gmail.com>
2017-11-21 18:06:59 +0200

    Update gradle version

--- sunshine/build/generated/source/r/debug/com/android/databinding/library/baseAdapters/R.java
+++ sunshine/build/generated/source/r/debug/com/android/databinding/library/baseAdapters/R.java
@@ -8,9 +8,9 @@ package com.android.databinding.library.baseAdapters;
 
 public final class R {
     public static final class id {
-        public static final int dataBinding = 0x7f0b0004;
-        public static final int onAttachStateChangeListener = 0x7f0b0009;
-        public static final int onDateChanged = 0x7f0b000a;
-        public static final int textWatcher = 0x7f0b0010;
+        public static final int dataBinding = 0x7f07002c;
+        public static final int onAttachStateChangeListener = 0x7f070058;
+        public static final int onDateChanged = 0x7f070059;
+        public static final int textWatcher = 0x7f070090;
     }
 }
